60d2d065009ce38264003ddabbddfbb03a4609cf
Kedar Iyer <vixidev99@protonmail.com>
2024-08-18 23:31:08 -0400

    untested refund logic

--- htlc.js
+++ htlc.js
@@ -159,3 +159,62 @@ function redeemHTLC(options) {
   return psbt.extractTransaction().toHex()
 }
   
+// TODO: Add some fee logic
+/**
+ * Produces a raw transaction to redeem an existing HTLC
+ * @param {Object} options
+ * @param {String} options.refundWIF      Private key of refund address in WIF format
+ * @param {String} options.refundAddress  Public address of refunder
+ * @param {String} options.witnessScript  Witness script for HTLC in hex format. If you used createHTLC, you can grab it from there, or you will have to ask your counterparty for it.
+ * @param {String} options.txHash         Transacion hash with the HTLC output you want to unlock
+ * @param {Number} options.value          The number of sats locked in the HTLC. IMPORTANT: If you send too low a number, the remainder of your sats will be burned. Proceed with caution. Test your code on regtest before using it in production.
+ * @param {Number} options.vout           (optional)(default: 0): the index number of the UTXO in txHash to use. will default to the first output. Specify an index number if you want to use a different output. 
+ * @return {String}                       Raw refund transaction to broadcast to network. send it with `bitcoin-cli sendrawtransaction <transaction>`
+*/
+function refundHTLC(options) {
+  const vout = options.vout || 0
+  const refundKeypair = ECPair.fromWIF(options.refundWIF)
+
+  // This is equivaluent to OP_0 OP_20 WITNESS_SCRIPT_HASH
+  const witnessScript = Buffer.from(options.witnessScript, 'hex')
+  const witnessScriptHash = bitcoin.crypto.sha256(witnessScript)
+  const witnessUtxoScript = Buffer.concat([Buffer.from([0x00, 0x20]), witnessScriptHash])
+
+  // Segwit transactions require you to use Psbt to sign (afaik)
+  const psbt = new bitcoin.Psbt({ network: bitcoin.networks[options.network] })
+  psbt.addInput({
+    hash: options.txHash,
+    index: vout, 
+    witnessScript: Buffer.from(options.witnessScript, 'hex'),
+    witnessUtxo: {
+      script: witnessUtxoScript,
+      value
+    }
+  })
+  const txFee = 2000;
+  psbt.addOutput({
+    address: options.refundAddress,
+    value: (value - txFee),
+  })
+
+  psbt.signInput(0, refundKeypair)
+
+  const sig = psbt.data.inputs[0].partialSig[0].signature
+
+  const witnessStack = [
+    sig,
+    refundKeypair.publicKey,
+    Buffer.from([]), // Segwit OP_FALSE is minimal, meaning no data is included
+    witnessScript
+  ]
+
+  // serialize witness
+  let finalScriptWitness = Buffer.from([0x04]) // 1-byte: Number of items
+  for (let i=0; i < 4; i++) {
+    finalScriptWitness = Buffer.concat([ finalScriptWitness, new Uint8Array([witnessStack[i].length]), witnessStack[i] ])
+  }
+
+  psbt.updateInput(0, { finalScriptWitness })
+
+  return psbt.extractTransaction().toHex()
+}
